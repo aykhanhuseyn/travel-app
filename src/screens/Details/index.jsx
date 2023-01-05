236787cc82e1a2237c874666b3a80f3808a3c440
Aykhan Huseyn <aykhanhuseyn@Aykhans-MacBook-Pro.local>
2023-01-06 03:37:54 +0400

    feat: update details view

--- src/screens/Details/index.jsx
+++ src/screens/Details/index.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
-import {View} from 'react-native';
-import {useRoute} from '@react-navigation/native';
+import {
+  Image,
+  ImageBackground,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import Typography from '../../components/Typography';
+import styles from './styles';
 
 const Details = () => {
+  const navigation = useNavigation();
   const {
     params: {item},
   } = useRoute();
-  //
+  const image = item?.images?.[0] ?? null;
+
   return (
-    <View>
+    <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+      <ImageBackground style={styles.image} source={{uri: image}}>
+        <View style={styles.imageHeader}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.goBack();
+            }}
+            style={styles.iconContainer}>
+            <Image
+              style={styles.icon}
+              source={require('../../assets/icons/arrow-left.png')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconContainer}>
+            <Image
+              style={styles.icon}
+              source={require('../../assets/icons/share.png')}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.imageFooter}>
+          {item?.images?.map(url => (
+            <Image key={url} source={{url}} style={styles.footerItem} />
+          ))}
+        </View>
+      </ImageBackground>
       <Typography.Title>{item?.name}</Typography.Title>
-    </View>
+    </ScrollView>
   );
 };
 
--- src/screens/Details/styles.js
+++ src/screens/Details/styles.js
@@ -1,5 +1,54 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const styles = StyleSheet.create({});
+const {width} = Dimensions.get('window');
+const sideSpacing = 32;
+const imageWidth = width - 2 * sideSpacing;
+
+const styles = StyleSheet.create({
+  container: {
+    margin: sideSpacing,
+  },
+  image: {
+    width: imageWidth,
+    height: Math.round(imageWidth * 1.24),
+    marginBottom: 48,
+    borderRadius: 20,
+    overflow: 'hidden',
+    justifyContent: 'space-between',
+  },
+  imageHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  iconContainer: {
+    width: 42,
+    height: 42,
+    marginHorizontal: 20,
+    marginVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.white,
+    borderRadius: 21,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+  },
+  imageFooter: {
+    flexDirection: 'row',
+    width: imageWidth - 48,
+    paddingHorizontal: 24,
+    paddingVertical: 8,
+    height: 64,
+    backgroundColor: 'rgba(255, 255, 255, 0.35)',
+    borderRadius: 16,
+  },
+  footerItem: {
+    width: 48,
+    height: 48,
+  },
+});
 
 export default styles;
